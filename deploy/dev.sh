082c4ed2148bcbc40be24f9c755bb44aa27d4971
sivakumar Vaivelu <sivakumar.bdu@gmail.com>
2019-10-15 14:13:12 +0530

    Add dev env

---
+++ deploy/dev.sh
@@ -0,0 +1,19 @@
+
+echo "Setting vars...."
+SERVER_IP=127.0.0.1
+SERVER_USER=root
+DOCKER_MACHINE_NAME=machine01
+GIT_DEPLOY_BRANCH=master
+
+COMPOSE_DEPLOY_FILE=docker-compose.development.yml
+STACK_NAME=trybank-stack
+DOCKER_FILE=Dockerfile.dev
+
+REPOS[0]=repourl/repo1:nginx.dev
+REPOS[1]=repourl/repo2:dev
+
+REGISTRY_LOGIN=$(docker login)
+SERVICES[0]=web
+SERVICES[1]=api
+
+SERVICE_NGINX=nginx
